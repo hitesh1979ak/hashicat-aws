4e0ad50fe24c7946d46c27783c07fe34e6415fe4
hitesh1979ak <48229742+hitesh1979ak@users.noreply.github.com>
2023-03-30 14:13:49 +1100

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "hitesh1979655-my-s3-bucket"
   acl    = "private"
 
   versioning = {
